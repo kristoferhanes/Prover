fa3a3f312909323b933d7e49a381e91a7c5d879b
Kristofer Hanes <kristoferhanes@gmail.com>
2015-11-15 01:29:02 -0800

    Initial Commit

---
+++ Prover/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prover
+//
+//  Created by Kristofer Hanes on 2015 11 15.
+//  Copyright © 2015 Kristofer Hanes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
